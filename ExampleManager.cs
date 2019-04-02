d099f3f6a12f57d22a346db940d9c7d6e4ac723e
AustinGarrettRichards <austingarrettrichards@gmail.com>
2019-04-01 23:41:30 -0600

    Commented and removed imports

--- ExampleManager.cs
+++ ExampleManager.cs
@@ -1,23 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ExampleManager
 {
+    //Constructor
     public ExampleManager(ConnectionHandler connectionHandler)
     {
+        //Add our function to the event
         connectionHandler.NotifyPacketReceived += receivePacket;
     }
 
+    //Event function for receiving packets.
     public void receivePacket(int packetId, Packet packet)
     {
         if(packetId == 1)
         {
-            ToServerPacket_1 data = (ToServerPacket_1) packet;
+            ExamplePacket_1 data = (ExamplePacket_1) packet;
             doAction(data.index, data.sentence, data.floats);
         }
     }
 
+    //Action for packet 1
     public void doAction(int index, string sentence, float[] floats)
     {
         Debug.Log("Action completed. Index:" + index + " Sentence:" + sentence + " floats length:" + floats.Length);
