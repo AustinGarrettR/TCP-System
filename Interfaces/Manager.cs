8d8755e003a300d5bae7242a83adc7e4aafcbc49
AustinGarrettRichards <austingarrettrichards@gmail.com>
2019-04-02 00:38:58 -0600

    Reorganized and added manager abstract class

---
+++ Interfaces/Manager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Manager
+{
+    public abstract void init();
+    public abstract void update();
+    public abstract void shutdown();
+
+}
