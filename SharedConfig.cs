8e1f72046de8095e7273fbcc87eb611058d0b388
AustinGarrettRichards <austingarrettrichards@gmail.com>
2019-04-02 23:11:50 -0600

    Add config

---
+++ SharedConfig.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+public class SharedConfig
+{
+    
+    //Networking
+    public static readonly byte ESCAPE = 0xBC;
+    public static readonly byte DELIMITER = 0x00;
+    public static readonly int TCP_PORT = 45955;
+    public static readonly int MAX_BUFFER_SIZE = 1024;
+
+}
